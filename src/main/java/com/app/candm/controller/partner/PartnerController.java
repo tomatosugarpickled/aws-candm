76cae2706013763091014f706ea10818fe12536b
tomatosugarpickled <tjdgh1851@gmail.com>
2026-03-03 00:54:02 +0900

    path fix

--- src/main/java/com/app/candm/controller/partner/PartnerController.java
+++ src/main/java/com/app/candm/controller/partner/PartnerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-@RequestMapping("/team/**")
+@RequestMapping("/team")
 @RequiredArgsConstructor
 public class PartnerController {
 
@@ -26,6 +26,6 @@ public class PartnerController {
         }
 
         model.addAttribute("memberId", memberId);
-        return "/team/partner-list/partner-list";
+        return "team/partner-list/partner-list";
     }
 }
